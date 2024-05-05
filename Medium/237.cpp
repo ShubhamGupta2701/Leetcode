1ddcd979ea8eb1c8de79c9940711f227f25f832c
Shubham Gupta <shubham272101@gmail.com>
2024-05-05 15:25:19 +0530

    add 237-medium-problem

---
+++ Medium/237.cpp
@@ -0,0 +1,9 @@
+// 237. Delete Node in a Linked List
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
