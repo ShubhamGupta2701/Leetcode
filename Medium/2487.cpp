bd2e8484f863bcc1a7c0b728ac2dc472e428174a
Shubham Gupta <shubham272101@gmail.com>
2024-05-06 10:00:20 +0530

    add 2487-medium problem

---
+++ Medium/2487.cpp
@@ -0,0 +1,18 @@
+// 2487. Remove Nodes From Linked List
+
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* node = head;
+        ListNode* nxtGreater = removeNodes(node->next);
+
+        node->next = nxtGreater;
+        if (nxtGreater == nullptr || node->val >= nxtGreater->val) {
+            return node;
+        }
+        return nxtGreater;
+    }
+};
