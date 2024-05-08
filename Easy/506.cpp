e9fabe8279cd33ee49d1d3bc110dcf21c396e696
Shubham Gupta <shubham272101@gmail.com>
2024-05-08 19:37:14 +0530

    add optimal solution

--- Easy/506.cpp
+++ Easy/506.cpp
@@ -21,3 +21,16 @@ public:
         return result;
     }
 };
+
+
+
+// Optimal and easy solution
+
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        return nums2[0] - nums1[0];
+    }
+};
