068b5ea5e1fe09fa8527e0266191305094ad8d45
Shubham Gupta <shubham272101@gmail.com>
2024-05-15 02:46:53 +0530

    add 231 easy problem

---
+++ Easy/231.cpp
@@ -0,0 +1,16 @@
+// 231. Power of Two 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        
+        for(int i =0;i<31;i++){
+        int ans = pow(2,i);
+            
+            if(ans == n)return true;
+
+        }
+        return false;
+    }
+};
