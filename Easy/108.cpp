b3a0a080e67ec627d08ad0e70a604290dae40612
Shubham Gupta <shubham272101@gmail.com>
2024-05-15 02:19:02 +0530

    add 108 easy problem

---
+++ Easy/108.cpp
@@ -0,0 +1,21 @@
+
+// 108. Convert Sorted Array to Binary Search Tree
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode* sortedArrayToBSTHelper(const vector<int>& nums, int start, int end) {
+        if (start > end) {
+            return nullptr;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
+        root->right = sortedArrayToBSTHelper(nums, mid + 1, end);
+        return root;
+    }
+};
