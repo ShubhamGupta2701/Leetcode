90ed0ce879de214c33063b1e86bb0f2d5cd6f214
Shubham Gupta <shubham272101@gmail.com>
2024-05-17 09:28:16 +0530

    add 3114-easy-problem

---
+++ Easy/3114.cpp
@@ -0,0 +1,25 @@
+// 3114. Latest Time You Can Obtain After Replacing Characters
+
+class Solution {
+public:
+    string findLatestTime(string s) {
+        if(s[0] == '?'){
+            if(s[1]!='0' && s[1]!='1'&&s[1]!='?'){
+                s[0] = '0';
+            }else{
+                s[0] = '1';
+            }
+        }
+        if(s[1] == '?'){
+            if(s[0] == '1'){
+                s[1] = '1';
+            }else{
+                s[1] = '9';
+            }
+        }
+        if(s[3]=='?') s[3] = '5';
+        if(s[4] == '?') s[4] = '9';
+
+        return s;
+    }
+};
