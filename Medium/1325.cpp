b296b913c4fca48812d6280bed4b4eaffb53c0ea
Shubham Gupta <shubham272101@gmail.com>
2024-05-17 09:25:52 +0530

    add 1325-medium-problem

---
+++ Medium/1325.cpp
@@ -0,0 +1,14 @@
+// 1325. Delete Leaves with a given value
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root) return nullptr;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (!root->left && !root->right && root->val == target)
+            return nullptr;
+        return root;
+
+    }
+};
