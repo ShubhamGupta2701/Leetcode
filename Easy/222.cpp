6f79347fc2a9ded820e9d9732d4a8ea5fe2eb4b1
Shubham Gupta <shubham272101@gmail.com>
2024-05-24 16:48:00 +0530

    add 222-easy-problem

---
+++ Easy/222.cpp
@@ -0,0 +1,13 @@
+// 222. Count Complete Tree Nodes
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        else{
+            return 1+countNodes(root->left)+countNodes(root->right);
+        }
+    }
+};
