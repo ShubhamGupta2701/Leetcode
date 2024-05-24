065e90a36643edb54a40f002f10851372ad742f3
Shubham Gupta <shubham272101@gmail.com>
2024-05-24 16:21:41 +0530

    add 303-easy-problem

---
+++ Easy/303.cpp
@@ -0,0 +1,19 @@
+// 303. Range Sum Query - Immutable
+
+
+class NumArray {
+public:
+    vector<int> PreSum;
+    NumArray(vector<int>& nums) {
+        PreSum.resize(nums.size());
+        PreSum[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            PreSum[i] = PreSum[i-1] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if(left == 0) return PreSum[right];
+        else return PreSum[right] - PreSum[left-1];
+    }
+};
