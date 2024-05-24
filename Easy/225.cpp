7c7932caff9ecc1698115569ebf1c9aa586b28c0
Shubham Gupta <shubham272101@gmail.com>
2024-05-24 16:50:19 +0530

    add 225-easy-problem

---
+++ Easy/225.cpp
@@ -0,0 +1,35 @@
+// 225. Implement Stack using Queue
+
+class MyStack {
+public:
+    queue<int> que;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push(x);
+        int n = que.size(); 
+
+        for(int i=0; i<n-1; i++){
+            que.push(que.front());
+            que.pop();
+        }
+    }
+    
+    int pop() {
+        int result = que.front();
+        que.pop();
+
+        return result;
+    }
+    
+    int top() {
+        return que.front();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
