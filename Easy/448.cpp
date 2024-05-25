c9d07a27678a535345936926c829a109fb52256e
Shubham Gupta <shubham272101@gmail.com>
2024-05-25 16:20:29 +0530

    add 448-easy-problem

---
+++ Easy/448.cpp
@@ -0,0 +1,21 @@
+// 448. Find All Numbers Disappeared in an Array
+
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        //vector<bool> ;
+        vector<int> ans;
+
+        for (const int num : nums) {
+            const int index = abs(num) - 1;
+            nums[index] = -abs(nums[index]);
+        }
+
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] > 0)
+                ans.push_back(i + 1);
+
+        return ans;
+    }
+};
