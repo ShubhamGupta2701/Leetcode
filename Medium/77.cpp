f10b9d9ed508a7d084cff649d007a2af2c407c45
Shubham Gupta <shubham272101@gmail.com>
2024-05-28 19:24:07 +0530

    add 77-medium-problem

---
+++ Medium/77.cpp
@@ -0,0 +1,26 @@
+// 77. Combinations
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> comb;
+
+        backtrack(1, comb, res, n, k);
+        return res;        
+    }
+
+private:
+    void backtrack(int start, vector<int>& comb, vector<vector<int>>& res, int n, int k) {
+        if (comb.size() == k) {
+            res.push_back(comb);
+            return;
+        }
+
+        for (int num = start; num <= n; num++) {
+            comb.push_back(num);
+            backtrack(num + 1, comb, res, n, k);
+            comb.pop_back();
+        }
+    }    
+};
