f9390045fb42d4e4ab0f1a70ea498f88b04bc52d
Shubham Gupta <shubham272101@gmail.com>
2024-05-29 10:44:17 +0530

    122-medium-problem

---
+++ Medium/122.cpp
@@ -0,0 +1,18 @@
+// 122. Best Time to Buy and Sell Stock II
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int start = prices[0];
+        int len = prices.size();
+        for(int i = 1;i<len; i++){
+            if(start < prices[i]){
+                max += prices[i] - start;
+            }
+            start = prices[i];
+        }
+        return max;
+    }
+};
