8de141e8fd04ce03772136e0290a00700b606248
Shubham Gupta <shubham272101@gmail.com>
2024-06-02 22:08:57 +0530

    add 704-easy-prob

---
+++ Easy/704.cpp
@@ -0,0 +1,29 @@
+// 704. Binary Search 
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
