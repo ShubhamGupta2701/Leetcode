b2eeeed1b044f8fdfac9a4a154be0c1d58f39c7f
Shubham Gupta <shubham272101@gmail.com>
2024-07-22 22:36:11 +0530

    add 2418-easy-problem

---
+++ Easy/2418.cpp
@@ -0,0 +1,21 @@
+// 2418. Sort the People
+
+class Solution {
+public:
+    vector<std::string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+        unordered_map<int, string> mapping;
+
+        for (int i = 0; i < n; ++i) {
+            mapping[heights[i]] = names[i];
+        }
+
+        sort(heights.rbegin(), heights.rend());
+
+        for (int i = 0; i < n; ++i) {
+            names[i] = mapping[heights[i]];
+        }
+
+        return names;
+    }
+};
