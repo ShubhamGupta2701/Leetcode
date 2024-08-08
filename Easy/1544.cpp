40a6cfcfdc6be640889c349f3d250a69dbadf734
Shubham Gupta <shubham272101@gmail.com>
2024-08-08 23:28:20 +0530

    add 1544-easy-problem

---
+++ Easy/1544.cpp
@@ -0,0 +1,19 @@
+// 1544. Make the String Great 
+
+
+class Solution {
+ public:
+  string makeGood(string s) {
+    string ans;
+    for (const char c : s)
+      if (!ans.empty() && isBadPair(ans.back(), c))
+        ans.pop_back();
+      else
+        ans.push_back(c);
+    return ans;
+  }
+
+  bool isBadPair(char a, char b) {
+    return a != b && tolower(a) == tolower(b);
+  }
+};
