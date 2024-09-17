08d330ff3650638014d670425e40418661f3b2b4
Shubham Gupta <shubham272101@gmail.com>
2024-09-17 22:51:44 +0530

    add 1-easy-problem

---
+++ Easy/1.cpp
@@ -0,0 +1,20 @@
+// 1. Two Sum 
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int size = nums.size();
+        for(int i =0;i<size;i++){
+            for(int j = i+1;j<size;j++){
+                if((nums[i] + nums[j] )== target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
