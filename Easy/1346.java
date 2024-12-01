4df3f620d183bcccced1caacc9d487b458091a6c
Shubham Gupta <shubham272101@gmail.com>
2024-12-01 19:56:44 +0530

    add 1346-easy-problem

---
+++ Easy/1346.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((arr[i] == arr[j]*2) && i!=j) return true;
+            }
+        } 
+        return false;
+    }
+}
