b5d8f377babb7bf187353a30455a08f62460efc5
Shubham Gupta <shubham272101@gmail.com>
2025-05-28 15:26:50 +0530

    add 2894-easy-problem

---
+++ Easy/2894.cpp
@@ -0,0 +1,16 @@
+// Divisible and Non-divisible Sums Difference
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=0, num2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m != 0){
+                num1 += i;
+            }else{
+                num2 += i;
+            }
+        }
+        return num1-num2;
+    }
+};
